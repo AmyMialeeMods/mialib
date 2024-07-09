a6df418447808c701e6cd0f39a11bbdc6842408d
ryanhcode <42245712+ryanhcode@users.noreply.github.com>
2024-07-08 20:20:07 -0400

    :wrench: Disable mixin that removes item durability

--- src/main/java/xyz/amymialee/mialib/mixin/ItemMixin.java
+++ src/main/java/xyz/amymialee/mialib/mixin/ItemMixin.java
@@ -10,20 +10,20 @@ import org.spongepowered.asm.mixin.injection.At;
 public class ItemMixin {
     @Mixin(Item.Settings.class)
     private static class ItemSettingsMixin {
-        @WrapWithCondition(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 0))
-        private <T> boolean mialib$noDurability1(Item.Settings instance, ComponentType<T> type, T value) {
-            if (value instanceof Integer i) {
-                return i != 0;
-            }
-            return true;
-        }
-
-        @WrapWithCondition(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 2))
-        private <T> boolean mialib$noDurability2(Item.Settings instance, ComponentType<T> type, T value) {
-            if (value instanceof Integer i) {
-                return i != 0;
-            }
-            return true;
-        }
+//        @WrapWithCondition(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 0))
+//        private <T> boolean mialib$noDurability1(Item.Settings instance, ComponentType<T> type, T value) {
+//            if (value instanceof Integer i) {
+//                return i != 0;
+//            }
+//            return true;
+//        }
+//
+//        @WrapWithCondition(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 2))
+//        private <T> boolean mialib$noDurability2(Item.Settings instance, ComponentType<T> type, T value) {
+//            if (value instanceof Integer i) {
+//                return i != 0;
+//            }
+//            return true;
+//        }
     }
 }
