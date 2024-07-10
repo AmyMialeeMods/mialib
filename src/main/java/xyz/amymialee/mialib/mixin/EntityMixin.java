a684323231fed4dfb07c9582540afb6fd4b1fee2
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2024-07-10 20:26:13 +0100

    version bump

--- src/main/java/xyz/amymialee/mialib/mixin/EntityMixin.java
+++ src/main/java/xyz/amymialee/mialib/mixin/EntityMixin.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.amymialee.mialib.events.MiaLibEvents;
 import xyz.amymialee.mialib.modules.ItemModule;
