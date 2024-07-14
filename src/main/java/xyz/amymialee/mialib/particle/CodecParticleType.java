03bb7b78c7f4ab30a6e9299f1f6cfa5d60d25ecd
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2024-07-14 17:31:19 +0100

    particle via function

--- src/main/java/xyz/amymialee/mialib/particle/CodecParticleType.java
+++ src/main/java/xyz/amymialee/mialib/particle/CodecParticleType.java
@@ -5,6 +5,9 @@ import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 public class CodecParticleType<T extends ParticleEffect> extends ParticleType<T> {
     private final MapCodec<T> codec;
@@ -14,6 +17,12 @@ public class CodecParticleType<T extends ParticleEffect> extends ParticleType<T>
         super(alwaysShow);
         this.codec = codec;
         this.packetCodec = packetCodec;
+    }
+
+    public CodecParticleType(boolean alwaysShow, @NotNull Function<ParticleType<T>, MapCodec<T>> codecGetter, @NotNull Function<ParticleType<T>, PacketCodec<? super RegistryByteBuf, T>> packetCodecGetter) {
+        super(alwaysShow);
+        this.codec = codecGetter.apply(this);
+        this.packetCodec = packetCodecGetter.apply(this);
     }
 
     @Override
