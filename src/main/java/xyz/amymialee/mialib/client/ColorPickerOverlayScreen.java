2e0096dafc195aee00a411b3bb73c7fe468cabab
yaya <70072204+absolutelyaya@users.noreply.github.com>
2025-07-07 02:03:17 +0200

    added HSV mode to Color Picker

--- src/main/java/xyz/amymialee/mialib/client/ColorPickerOverlayScreen.java
+++ src/main/java/xyz/amymialee/mialib/client/ColorPickerOverlayScreen.java
@@ -18,6 +18,7 @@ import org.joml.Vector3f;
 import xyz.amymialee.mialib.Mialib;
 import xyz.amymialee.mialib.mvalues.MValueColorWidget;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HexFormat;
 import java.util.List;
@@ -47,12 +48,14 @@ public class ColorPickerOverlayScreen extends Screen
 	protected void init()
 	{
 		super.init();
-		colorPicker = addDrawableChild(new ColorPickerWidget(width / 2 - 50, height / 2 - 50, getTitle(), colorPicker == null ? source.value.get() : colorPicker.value));
-		parent.init(client, width, height);
+		colorPicker = addDrawableChild(new ColorPickerWidget(width / 2 - 50, height / 2 - 50, getTitle(),
+				colorPicker == null ? source.value.get() : colorPicker.value, colorPicker != null && colorPicker.hsv));
+		if(client != null)
+			parent.init(client, width, height);
 		addDrawableChild(ButtonWidget.builder(Text.translatable("mialib.screen.mvalues.color_picker.confirm"), i -> close())
-								 .dimensions(width / 2 - 95, height / 2 + 54, 90, 20).build());
+								 .dimensions(width / 2 - 95, height / 2 + 68, 90, 20).build());
 		addDrawableChild(ButtonWidget.builder(Text.translatable("mialib.screen.mvalues.color_picker.reset"), i -> colorPicker.setValue(defaultValue))
-								 .dimensions(width / 2 + 5, height / 2 + 54, 90, 20).build());
+								 .dimensions(width / 2 + 5, height / 2 + 68, 90, 20).build());
 	}
 	
 	@Override
@@ -89,12 +92,14 @@ public class ColorPickerOverlayScreen extends Screen
 		
 		final List<ColorChannelSlider> channels = new ArrayList<>();
 		final TextFieldWidget hexInputField;
+		final ButtonWidget RGButton, HSButton;
 		ColorChannelSlider red, green, blue;
 		int value;
+		boolean hsv;
 		
-		public ColorPickerWidget(int x, int y, Text title, int startColor)
+		public ColorPickerWidget(int x, int y, Text title, int startColor, boolean hsv)
 		{
-			super(x, y, 100, 100, title);
+			super(x, y, 100, 112, title);
 			value = startColor;
 			channels.add(red = new ColorChannelSlider(x + 2, y + 36, width - 4, 20, ColorHelper.getRedFloat(startColor),
 					new Vector3f(1, 0, 0), this::onColorChanged));
@@ -110,8 +115,13 @@ public class ColorPickerOverlayScreen extends Screen
 						return false;
 				return true;
 			});
+			setHsv(hsv);
 			hexInputField.setPosition(x + 3, y + 15);
 			hexInputField.setChangedListener(this::setColorHex);
+			RGButton = ButtonWidget.builder(Text.of("RGB"), i -> setHsv(false)).dimensions(x + 27 - 3, y + 101 - 3, 25, 17).build();
+			HSButton = ButtonWidget.builder(Text.of("HSV"), i -> setHsv(true)).dimensions(x + 54 - 3, y + 101 - 3, 25, 17).build();
+			RGButton.setAlpha(0);
+			HSButton.setAlpha(0);
 			onColorChanged();
 		}
 		
@@ -131,8 +141,17 @@ public class ColorPickerOverlayScreen extends Screen
 			var matrices = context.getMatrices();
 			matrices.pushMatrix();
 			int x = getX(), y = getY();
-			context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, x - 3, y - 3, 0, 0, 106, 104, 106, 104);
+			matrices.pushMatrix();
+			matrices.translate(x - 3, y - 3);
 			TextRenderer tRender = MinecraftClient.getInstance().textRenderer;
+			context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, 0, 0, 0, 0, 106, 104, 106, 121);
+			context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, 27, 101, hsv ? 25 : 0, 104, 25, 17, 106, 121);
+			context.drawText(tRender, Text.of("RGB"), 31, 106, RGButton.isHovered() ? 0xffffffff : 0xff000000, false);
+			context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, 54, 101, hsv ? 0 : 25, 104, 25, 17, 106, 121);
+			context.drawText(tRender, Text.of("HSV"), 58, 106, HSButton.isHovered() ? 0xffffffff : 0xff000000, false);
+			matrices.popMatrix();
+			RGButton.render(context, mouseX, mouseY, delta);
+			HSButton.render(context, mouseX, mouseY, delta);
 			context.drawText(tRender, getMessage(), x + (width - tRender.getWidth(getMessage())) / 2, y - 14, 0xffffffff, true);
 			channels.forEach(i -> i.render(context, mouseX, mouseY, delta));
 			hexInputField.render(context, mouseX, mouseY, delta);
@@ -148,6 +167,10 @@ public class ColorPickerOverlayScreen extends Screen
 			channels.forEach(i -> i.onClick(mouseX, mouseY));
 			hexInputField.setFocused(hexInputField.isHovered());
 			hexInputField.onClick(mouseX, mouseY);
+			if(RGButton.isHovered())
+				RGButton.onClick(mouseX, mouseY);
+			if(HSButton.isHovered())
+				HSButton.onClick(mouseX, mouseY);
 		}
 		
 		@Override
@@ -189,13 +212,17 @@ public class ColorPickerOverlayScreen extends Screen
 		
 		void onColorChanged()
 		{
-			int fullColor = ColorHelper.fromFloats(1f, red.getValue(), green.getValue(), blue.getValue());
+			int fullColor;
+			if(hsv)
+				fullColor = ColorHelper.withAlpha(1f, Color.getHSBColor(red.getValue(), green.getValue(), blue.getValue()).getRGB());
+			else
+				fullColor = ColorHelper.fromFloats(1f, red.getValue(), green.getValue(), blue.getValue());
 			red.setFullColor(fullColor);
 			green.setFullColor(fullColor);
 			blue.setFullColor(fullColor);
-			hexInputField.setText(Integer.toHexString(fullColor).substring(2));
 			
-			value = ColorHelper.withAlpha(0, fullColor);
+			value = ColorHelper.withAlpha(0f, fullColor);
+			hexInputField.setText(Integer.toHexString(fullColor).substring(2));
 		}
 		
 		void setColorHex(String v)
@@ -203,15 +230,38 @@ public class ColorPickerOverlayScreen extends Screen
 			if(v.length() != 6)
 				return;
 			int color = HexFormat.fromHexDigits(v);
-			
-			red.setValue(ColorHelper.getRedFloat(color));
-			green.setValue(ColorHelper.getGreenFloat(color));
-			blue.setValue(ColorHelper.getBlueFloat(color));
+			if(value == color)
+				return;
+			updateSliders(color);
 			red.setFullColor(color);
 			green.setFullColor(color);
 			blue.setFullColor(color);
 			
 			value = color;
+		}
+		
+		void updateSliders(int color)
+		{
+			if(hsv)
+			{
+				float[] hsvFloats = Color.RGBtoHSB(ColorHelper.getRed(color), ColorHelper.getGreen(color), ColorHelper.getBlue(color), null);
+				red.setValue(hsvFloats[0]);
+				green.setValue(hsvFloats[1]);
+				blue.setValue(hsvFloats[2]);
+			}
+			else
+			{
+				red.setValue(ColorHelper.getRedFloat(color));
+				green.setValue(ColorHelper.getGreenFloat(color));
+				blue.setValue(ColorHelper.getBlueFloat(color));
+			}
+		}
+		
+		void setHsv(boolean hsv)
+		{
+			this.hsv = hsv;
+			channels.forEach(i -> i.setHSV(hsv));
+			updateSliders(value);
 		}
 		
 		@Override
@@ -227,6 +277,7 @@ public class ColorPickerOverlayScreen extends Screen
 			final Vector3f channel;
 			final Runnable onChangeValue;
 			int fullColor;
+			boolean hsv;
 			
 			public ColorChannelSlider(int x, int y, int width, int height, double value, Vector3f channel, Runnable onChangeValue)
 			{
@@ -278,10 +329,9 @@ public class ColorPickerOverlayScreen extends Screen
 				matrices.pushMatrix();
 				matrices.pushMatrix();
 				matrices.rotate((float)Math.toRadians(90));
-				int start = getPreviewColor(true);
-				int end = getPreviewColor(false);
-				context.fillGradient(getY() + 3, -getX() - width + 1, getY() + height - 3, -getX() - 1,
-						ColorHelper.withAlpha(alpha, start), ColorHelper.withAlpha(alpha, end));
+				drawPreviewGradient(context);
+				if(hsv && channel.x > 0f)
+					drawHueGradient(context, Math.min(alpha * channel.x, 1f));
 				matrices.popMatrix();
 				context.drawBorder(getX() + 1, getY() + 3, width - 2, height - 6, hovered ? 0xffffffff : 0xff000000);
 				context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, !hovered && !isFocused() ? HANDLE : HANDLE_HIGHLIGHTED,
@@ -291,8 +341,40 @@ public class ColorPickerOverlayScreen extends Screen
 				matrices.popMatrix();
 			}
 			
+			void drawPreviewGradient(DrawContext context)
+			{
+				int start = getPreviewColor(true);
+				int end = getPreviewColor(false);
+				context.fillGradient(getY() + 3, -getX() - width + 1, getY() + height - 3, -getX() - 1,
+						ColorHelper.withAlpha(alpha, start), ColorHelper.withAlpha(alpha, end));
+			}
+			
+			void drawHueGradient(DrawContext context, float alpha)
+			{
+				float[] hsv = Color.RGBtoHSB(ColorHelper.getRed(fullColor), ColorHelper.getGreen(fullColor), ColorHelper.getBlue(fullColor), null);
+				int r = Color.getHSBColor(0, hsv[1], hsv[2]).getRGB();
+				int g = Color.getHSBColor(0.66f, hsv[1], hsv[2]).getRGB();
+				int b = Color.getHSBColor(0.33f, hsv[1], hsv[2]).getRGB();
+				
+				context.fillGradient(getY() + 3, -getX() - width + 1, getY() + height - 3, -getX() - width / 3 * 2,
+						ColorHelper.withAlpha(alpha, r), ColorHelper.withAlpha(alpha, g));
+				
+				context.fillGradient(getY() + 3, -getX() - width / 3 * 2, getY() + height - 3, -getX() - width / 3,
+						ColorHelper.withAlpha(alpha, g), ColorHelper.withAlpha(alpha, b));
+				
+				context.fillGradient(getY() + 3, -getX() - width / 3, getY() + height - 3, -getX() - 1,
+						ColorHelper.withAlpha(alpha, b), ColorHelper.withAlpha(alpha, r));
+			}
+			
 			int getPreviewColor(boolean full)
 			{
+				if(hsv)
+				{
+					float[] hsvFloats = Color.RGBtoHSB(ColorHelper.getRed(fullColor), ColorHelper.getGreen(fullColor), ColorHelper.getBlue(fullColor), null);
+					int color = ColorHelper.lerp(full ? 1f : 1f - Math.min(channel.y, hsvFloats[2]), 0xffffff, fullColor); //sat
+					color = ColorHelper.lerp(full ? 1f : channel.y, 0, color); //val
+					return ColorHelper.withAlpha(1f, color);
+				}
 				float red = full ? Math.max(channel.x, ColorHelper.getRedFloat(fullColor)) : Math.min(1f - channel.x, ColorHelper.getRedFloat(fullColor));
 				float green = full ? Math.max(channel.y, ColorHelper.getGreenFloat(fullColor)) : Math.min(1f - channel.y, ColorHelper.getGreenFloat(fullColor));
 				float blue = full ? Math.max(channel.z, ColorHelper.getBlueFloat(fullColor)) : Math.min(1f - channel.z, ColorHelper.getBlueFloat(fullColor));
@@ -312,6 +394,11 @@ public class ColorPickerOverlayScreen extends Screen
 			public float getValue()
 			{
 				return (float)value;
+			}
+			
+			public void setHSV(boolean hsv)
+			{
+				this.hsv = hsv;
 			}
 		}
 	}
