0f289405c25b977de71fe6b71d543db4c804d448
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2025-12-11 23:14:12 +0000

    fix durability oops

--- src/main/java/xyz/amymialee/mialib/mixin/ItemMixin.java
+++ src/main/java/xyz/amymialee/mialib/mixin/ItemMixin.java
@@ -13,14 +13,14 @@ public class ItemMixin implements MItem {
     @Mixin(Item.Settings.class)
     private static class ItemSettingsMixin {
         @WrapOperation(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 0))
-        private <T> Item.Settings mialib$nomaxdamage(Item.Settings instance, ComponentType<T> type, T value, Operation<Item.Settings> original) {
-            if (value == (Integer) 0) return instance;
+        private <T> Item.Settings mialib$nomaxdamage(Item.Settings instance, ComponentType<T> type, T value, Operation<Item.Settings> original, int maxDamage) {
+            if (maxDamage == 0) return instance;
             return original.call(instance, type, value);
         }
 
         @WrapOperation(method = "maxDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;component(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Lnet/minecraft/item/Item$Settings;", ordinal = 2))
-        private <T> Item.Settings mialib$nodamage(Item.Settings instance, ComponentType<T> type, T value, Operation<Item.Settings> original) {
-            if (value == (Integer) 0) return instance;
+        private <T> Item.Settings mialib$nodamage(Item.Settings instance, ComponentType<T> type, T value, Operation<Item.Settings> original, int maxDamage) {
+            if (maxDamage == 0) return instance;
             return original.call(instance, type, value);
         }
     }
